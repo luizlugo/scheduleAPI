a1ea57a2531a7a8480f02ffd4303a333c5ccbfbe
Luis Lugo <bigboss769@hotmail.com>
2014-11-23 23:52:54 -0600

    adding user controller

---
+++ controllers/users.js
@@ -0,0 +1,15 @@
+//required files
+var usersController = (function(){
+	//----------------
+	// Private members
+	//----------------
+	function login(req, resp){
+		
+	}
+
+
+	//----------------
+	// Public API
+	//----------------
+
+})();
